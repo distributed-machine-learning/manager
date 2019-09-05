efebee70055ef4362011ea87cef928b89ffcfade
Syed Faraaz Ahmad <faraaz98@live.com>
2019-09-05 20:53:58 +0530

    add flask to docker compose

--- app/app.py
+++ app/app.py
@@ -6,4 +6,5 @@ def hello_world():
     return 'Flask installed!'
 
 if __name__ == '__main__':
+    print("App server running at localhost:3000")
     app.run(debug=True, host='0.0.0.0', port='3000')
--- run-master.sh
+++ run-master.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/bash
+
+# build image for 
+docker build -t app .
+docker tag app:latest app:staging
+
+# Run the compose file
+docker-compose up
